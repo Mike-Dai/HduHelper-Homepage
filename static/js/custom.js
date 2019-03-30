cfb250f00c880af6354a2f4d282b82f609cd5d41
Mike-Dai <mrmikedai@outlook.com>
2019-03-30 15:56:07 +0800

    Change the style of toggle button

--- static/js/custom.js
+++ static/js/custom.js
@@ -4,9 +4,11 @@ button.onclick = function() {
 	var timeline = document.getElementById("timeline");
 	if (timeline.style.display == 'block') {
 		timeline.style.display = 'none';
+		button.innerHTML = "<h3><br><br>更多</h3>"
 	}
 	else {
 		timeline.style.display = 'block';
+		button.innerHTML = "<h3><br><br>收起</h3>";
 	}
 };
 
